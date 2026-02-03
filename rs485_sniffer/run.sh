6dc2c2741e9d79fda91c0aed5f75fc5bca4426a2
aakaeret-30 <lotte.henrik.nielsen@gmail.com>
2026-02-03 14:29:31 +0100

    Update run.sh

--- rs485_sniffer/run.sh
+++ rs485_sniffer/run.sh
@@ -1,5 +1,2 @@
-#!/usr/bin/with-contenv bash
-set -e
-
-echo "[rs485_sniffer] Starting RS485 sniffer..."
-python3 /app/sniff_to_mqtt.py
+#!/usr/bin/env sh
+echo "[rs485_sniffer] container init done"
